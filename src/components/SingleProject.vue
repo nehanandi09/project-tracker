8e120dc7ae4d57261a8019fa72a8cdb44be2455d
Neha Nandi <nandineha09@gmail.com>
2022-08-14 18:46:55 +0100

    first commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="project" :class="{ complete: project.complete }">
+    <div class="actions">
+      <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+      <div ckass="icons">
+        <router-link :to="{ name: 'EditProject', params: { id: project.id } }">
+          <span class="material-icons">edit</span>
+        </router-link>
+        <span class="material-icons" @click="deleteProject">delete</span>
+        <span class="material-icons tick" @click="toggleComplete">done</span>
+      </div>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetails: false,
+      uri: "http://localhost:3000/projects/" + this.project.id,
+    };
+  },
+  methods: {
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" })
+        .then(() => this.$emit("delete", this.project.id))
+        .catch((err) => console.log(err));
+    },
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }),
+      })
+        .then(() => {
+          this.$emit("complete", this.project.id);
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3pc rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
+}
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h3 {
+  cursor: pointer;
+}
+.material-icons {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.material-icons:hover {
+  color: #777;
+}
+.project.complete {
+  border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+  color: #00ce89;
+}
+</style>
